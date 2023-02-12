97dac830ba2a00c6afa29be058529f98b86b05d7
faleialina <august3@mail.ru>
2023-02-12 21:34:29 +0300

    feature/dropdown-actions

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-//emtpt
+const img = document.querySelector('.dropdown');
+const dl = document.querySelector('.dropdown-list');
+
+
+img.addEventListener('click', function () {
+
+    if (dl.style.display === 'none') {
+        dl.style.display = 'flex';
+        document.querySelector('.img').style = 'background-image: url(.//assets/Chevron2.svg)';
+    } else {
+        dl.style.display = 'none';
+        document.querySelector('.img').style = 'background-image: url(.//assets/Chevron.svg)';
+    }
+})
+
+
+
